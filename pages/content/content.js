f94e71da801ef0399d7017d971125c16c2022ecb
maxiangguang <maxiangguang@sfhc.onaliyun.com>
2020-05-14 17:41:44 +0800

    数据绑定

---
+++ pages/content/content.js
@@ -0,0 +1,73 @@
+// pages/content/content.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: "hello world 1",
+    num: 12,
+    person:{
+      name: "zhangsan",
+      age: 12
+    },
+    title: "this is a title"
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
